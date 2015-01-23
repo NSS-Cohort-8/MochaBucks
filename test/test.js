6c640619f0b236af318830bcf58187dbb67332b1
Scott <scott@sscotth.io>
2015-01-23 14:16:30 -0600

    initial Constructor test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@
 
 'use strict';
 
-var expect = require('chai').expect;
+var expect      = require('chai').expect,
+    Constructor = require(process.cwd() + '/lib/Constructor');
 
 describe('Mocha', function () {
   it('should expect true and false', function () {
@@ -12,3 +13,11 @@ describe('Mocha', function () {
   });
 });
 
+describe('Constructor', function () {
+  describe('init', function () {
+    it('should have public variables', function () {
+      var constructed = new Constructor();
+      expect(constructed.publicVariable).to.equal('foo');
+    });
+  });
+});
